d83784917cc012d9057495f781c69e4acc9f9d85
Danrley <Danrleygja@gmail.com>
2024-09-05 22:19:44 -0300

    Primeira Aula

---
+++ src/main/java/com/fatecrl/contacorrente/controller/ContaCorrenteController.java
@@ -0,0 +1,12 @@
+package com.fatecrl.contacorrente.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/conta-corrente")
+public class ContaCorrenteController {
+
+  
+}
